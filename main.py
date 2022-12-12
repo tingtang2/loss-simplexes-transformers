daea973bd94a6fc4bb21ac9d86e44fcbe41d9f81
Ting Chen <zc2666@columbia.edu>
2022-12-12 04:05:16 +0000

    begin bleu metric and translations

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from torch.utils.data import DataLoader
 from torchtext.data.utils import get_tokenizer
 from torchtext.datasets import Multi30k
 from tqdm import tqdm
+from torchtext.data.metrics import bleu_score
 
 from data import get_dataset, get_text_transforms, get_vocabs
 from models.model import DecoderRNN, EncoderRNN, Seq2SeqTransformer
@@ -179,6 +180,44 @@ def evaluate(model, data, criterion, batch_size, device):
 
 # train and eval functions for rnn
 ##################################################################################################
+def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = tensorFromSentence(input_lang, sentence)
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
+
+        encoder_outputs = torch.zeros(max_length,
+                                      encoder.hidden_size,
+                                      device=device)
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei],
+                                                     encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
+
+        decoder_hidden = encoder_hidden
+
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoded_words.append('<EOS>')
+                break
+            else:
+                decoded_words.append(output_lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_words, decoder_attentions[:di + 1]
+
+
 def train_epoch_rnn(dataset, encoder, decoder, encoder_optimizer,
                     decoder_optimizer, criterion, device, batch_size):
     encoder.train()
@@ -233,6 +272,8 @@ def evaluate_rnn(dataset, encoder, decoder, criterion, device, batch_size):
             loss = criterion(
                 decoder_output.reshape(-1, decoder_output.size(-1)),
                 tgt[:, 1:].reshape(-1))
+
+            decoder_output = translate()
 
             running_loss += loss.item()
 
